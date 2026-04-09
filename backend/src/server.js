d32a586eb26b7255435dbe28b77351166395a251
Rahul Sharma <quake.05.005@gmail.com>
2026-04-10 04:09:01 +0530

    added database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import cors from "cors";
+import connectDB from './lib/db.js';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "https://convohub-app.vercel.app",
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+    connectDB();
 });
