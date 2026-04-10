a0a100cf02767ebeaf28a537c94250a4fb8859e0
Aayush <quake.05.005@gmail.com>
2026-04-10 23:29:31 +0530

    optimized

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,7 +2,11 @@ import jwt from 'jsonwebtoken';
 
 
 export const generateToken = (UserId,res) => {
-    const token = jwt.sign({ id: UserId }, process.env.JWT_SECRET,{expiresIn: '7d'});
+    const { JWT_SECRET } = process.env;
+    if(!JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    const token = jwt.sign({ id: UserId }, JWT_SECRET,{expiresIn: '7d'});
     res.cookie('jwt', token, {
         httpOnly: true, // client will get token only through http request, not accessible via js
         maxAge: 7 * 24 * 60 * 60 * 1000 ,// 7 days
