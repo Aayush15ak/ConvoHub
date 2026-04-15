b9209e9776e3f334f856f1eecdadd85dec73f3e6
Aayush <quake.05.005@gmail.com>
2026-04-15 23:08:49 +0530

    added basic frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import ChatPage from './pages/ChatPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from  './pages/SignupPage.jsx'  
 
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold text bg-purple-500">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+        <Route path="/" element={<ChatPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
+
+    </Routes>
   )
 }
 
