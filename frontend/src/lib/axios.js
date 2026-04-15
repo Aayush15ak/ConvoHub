af305d608cb865dfe1b540a42ff328268531b1f2
Aayush <quake.05.005@gmail.com>
2026-04-15 23:53:48 +0530

    axios api and zustand state management

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
-// now in development, we will use this file to create an axios instance with the base URL of our backend API. This way, we can easily make API calls throughout our application without having to specify the base URL every time.
-//TO-DO: Replace it wil deployed backend URL before deployement
-
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api', // use env variable or fallback to localhost
     withCredentials: true, // to send cookies with every request
 })
