74a56ee97f5e83e9fd9e4ecca1c6e225abfdf7b7
Aayush <quake.05.005@gmail.com>
2026-04-26 05:17:02 +0530

    debug

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api', // use env variable or fallback to localhost
+    baseURL: 'https://convohub-4shb.onrender.com/api' || 'http://localhost:3000/api', // use env variable or fallback to localhost
     withCredentials: true, // to send cookies with every request
 })
 
