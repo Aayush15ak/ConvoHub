5be5bdf645c11a6d867386a05a8c44cfeabd1143
Aayush <quake.05.005@gmail.com>
2026-04-26 05:37:07 +0530

    debug 2 deployment

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,8 +10,8 @@ export const generateToken = (UserId,res) => {
     res.cookie('jwt', token, {
         httpOnly: true, // client will get token only through http request, not accessible via js
         maxAge: 7 * 24 * 60 * 60 * 1000 ,// 7 days
-        sameSite : 'strict', // prevent CSRF
-        secure : process.env.NODE_ENV === 'production' ? true : false// set secure flag in production
+        sameSite: 'none',
+        secure : true // set secure flag in production
 
     })
     return token;
